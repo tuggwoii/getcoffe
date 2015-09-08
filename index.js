820d9302c6149c7ad3c974f42d8878242cff7439
Tak <tugg.solo@gmail.com>
2015-09-08 21:10:15 +0700

    video

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname, 'css'));
+app.use(express.static(__dirname, 'media'));
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
@@ -12,6 +13,9 @@ app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+app.get('/v', function(request, response) {
+  response.render('pages/v');
 });
 
 app.listen(app.get('port'), function() {
